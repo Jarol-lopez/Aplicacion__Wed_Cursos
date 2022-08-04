6ed7aeb3701d2224549bb80a1dbc3a9e0127e879
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-04 10:58:59 -0600

    Create Informacion.php

---
+++ Secciones/Informacion.php
@@ -0,0 +1,21 @@
+<?PHP INCLUDE('../templates/cabeceras.php'); ?>
+Misión, visión y valores
+
+
+
+-> SEAS tiene como objetivo ser un centro reconocido de formación online entre los estudiantes. 
+
+-> Pretendemos que en Campus SEAS todos los alumnos, la comunidad que lo conforma, sean los auténticos protagonistas de su formación, y que sirva para crear profesionales reconocidos en las empresas y que favorezca su desarrollo profesional y personal. 
+
+Podríamos destacar como nuestros valores:
+
+-> Una excelente atención al alumno.
+-> El empleo del mejor material de estudio y aprendizaje.
+-> La experiencia profesional y docente de nuestro profesorado.
+-> Nuestros acuerdos de colaboración con empresas de diversos sectores.
+-> Garantía de calidad universitaria de los estudios.
+-> La ilusión y compromiso del equipo.
+-> El uso de una metodología abierta, apoyada en las nuevas tecnologías.
+
+
+<?PHP INCLUDE('../templates/pie_pagina.php'); ?>
