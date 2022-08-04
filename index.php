eba400065cf631fae99a5ac36d1487f65cca6d28
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-04 10:58:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+  <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,13 +26,17 @@
             <a href="#" class="logo">
                 <img src="images/LogoVirtA.png"/>
             </a>
-
             <ul class="menu">
+                
                 <li><a href="#" class="active">Inicio</a></li>
-                <li><a href="#">Cursos</a></li>
-                <li><a href="#">Iniciar Sesión</a></li>
-                <li><a href="#">Regístrate</a></li>
+                <form action="Secciones/login.php" method="post">
+                <li><button><a href="#" class="active">Iniciar Seccion</a></button></li>
+                </form>
+                <form action="Secciones/vistas_cusos.php" method="post">
+                <li><button><a href="#" class="active">Cursos</a></button></li>
+                </form>
             </ul>
+           
         </nav>
         <div class="main-heading">
             <h1>La Mejor Academia Virtual</h1>
@@ -99,4 +117,14 @@
         </div>
     </section>
 </body>
+</html>
+   
+    
+</div>
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+  </body>
 </html>
