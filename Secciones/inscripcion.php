f948e818c06e506d3791ec7371bbc00352c537c6
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-18 02:38:49 -0600

    Update inscripcion.php

--- Secciones/inscripcion.php
+++ Secciones/inscripcion.php
@@ -1,18 +1,18 @@
 
-<?php include('../Secciones/vista_inscripcion.php') ?>
+<?php  ?>
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Registro</title>
+    <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css"/>
+
   </head>
-  <body background="../images/login.jpg ">
+  <body>
 
   <div class="container">
     <div class="row">
