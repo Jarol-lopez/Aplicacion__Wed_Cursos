4703fd74213ad814143b55eaaf14e39541902509
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-18 02:38:53 -0600

    Update login.php

--- Secciones/login.php
+++ Secciones/login.php
@@ -1,67 +1,68 @@
+<?php 
+session_start(); 
+if($_POST){
+  if($_POST['usuario'] == 'admin' && $_POST['contrasenia'] == 'admin'){
+    $_SESSION['username'] = 'admin';
+   header('Location: ../Secciones/Informacion.php');
+  }else{
 
+    $mensaje = 'Usuario o contraseña incorrectos';
+  }
+  
+}
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Login</title>
+    <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css"/>
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
-  <body background="../images/login.jpg ">
+  <body>
+    <div class="container">
 
-  <div class="container">
-    <div class="row">
-    <div class="col-md-5">
-        
-      </div>
-      <div class="col-md-5">
-       
-        <div class="card">
-            <div class="card-header">
-                Inicio de Sesión
-            </div>
-            <div class="card-body">
-                <div class="mb-3">
-                  <label for="" class="form-label">USUARIO</label>
-                  <input type="text"
-                    class="form-control" 
-                    name="USUARIO" 
-                    id="USUARIO" 
-                    aria-describedby="helpId" placeholder="Usuario">
-                  <small id="helpId" class="form-text text-muted">Escriba su Usuario</small>
-                </div>
-                <div class="mb-3">
-                  <label for="" class="form-label">CONTRASEÑA</label>
-                  <input type="password"
-                    class="form-control" name="" id="" aria-describedby="helpId" placeholder="password">
-                  <small id="helpId" class="form-text text-muted">Escriba su Contraseña </small>
-
-                </div>
-                <form action="../Secciones/vista_alumnos.php" method="post">
-                  <button type="submit" class="btn btn-primary">Iniciar Sesión </button>
-                  <br>
-                  </form>
-                  <br>
-                  <form action="../Secciones/inscripcion.php" method="post">
-                  <button type="submit" class="btn btn-danger">Registrate </button>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="card">
+<form action="" method="post">
+                    <div class="card-header">   Inicio se sesión    </div>
+                <div class="card-body">
                   
-                  </form>
+                <?php if(isset($mensaje)) {?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $mensaje; ?>
+                        </div>
+                    <?php } ?>
+                  
+                <div class="mb-3">
+                  <label for="usuario" class="form-label">Usuario</label>
+                  <input type="text"
+                    class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Usuario">
+                </div>
+                <div class="mb-3">
+                  <label for="contrasenia" class="form-label">Contraseña</label>
+                  <input type="password"
+                    class="form-control" name="contrasenia" id="contrasenia" aria-describedby="helpId" placeholder="">
+                </div>
+                <button type="submit" class="btn btn-primary">Entrar a la aplciación</button>
+                </form>
+                </div>
+
             </div>
-            
-            
             </div>
         </div>
-    
-</div>
+    </div>
+
+  
+
 
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   </body>
 </html>
-<?PHP INCLUDE('../templates/pie_pagina.php'); ?>
