d9198f49b174fa32bf3e3c2efb2bd8d0294f6622
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-18 02:38:44 -0600

    Update Informacion.php

--- Secciones/Informacion.php
+++ Secciones/Informacion.php
@@ -1,32 +1,21 @@
 <?PHP INCLUDE('../templates/cabeceras.php'); ?>
-
-
 Misión, visión y valores
 
-<br>
 
--> VirtAcad tiene como objetivo ser un academia virtual reconocida de formación online entre los estudiantes. 
- <br>   
--> Pretendemos que en VirtAcad todos los alumnos, la comunidad que lo conforma, sean los auténticos protagonistas de su formación, y que sirva para crear profesionales 
-<br>
-reconocidos en las empresas y que favorezca su desarrollo profesional y personal. 
-<br>
+
+-> SEAS tiene como objetivo ser un centro reconocido de formación online entre los estudiantes. 
+
+-> Pretendemos que en Campus SEAS todos los alumnos, la comunidad que lo conforma, sean los auténticos protagonistas de su formación, y que sirva para crear profesionales reconocidos en las empresas y que favorezca su desarrollo profesional y personal. 
+
 Podríamos destacar como nuestros valores:
-<br>
+
 -> Una excelente atención al alumno.
-<br>
 -> El empleo del mejor material de estudio y aprendizaje.
-<br>
 -> La experiencia profesional y docente de nuestro profesorado.
-<br>
 -> Nuestros acuerdos de colaboración con empresas de diversos sectores.
-<br>
 -> Garantía de calidad universitaria de los estudios.
-<br>
 -> La ilusión y compromiso del equipo.
-<br>
 -> El uso de una metodología abierta, apoyada en las nuevas tecnologías.
-<br>
 
 
 <?PHP INCLUDE('../templates/pie_pagina.php'); ?>
