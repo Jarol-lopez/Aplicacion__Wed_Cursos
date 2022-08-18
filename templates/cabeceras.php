1014497f83094f14ab4134cf1b7288fea324429e
Jlopez76 <62785509+Jarol-lopez@users.noreply.github.com>
2022-08-18 02:39:08 -0600

    Update cabeceras.php

--- templates/cabeceras.php
+++ templates/cabeceras.php
@@ -8,22 +8,21 @@
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css"/>
+
   </head>
-  <body background="../images/ult.jpg ">
+  <body>
     
   <nav class="navbar navbar-expand navbar-light bg-light">
     
            <div class="nav navbar-nav">
-               <a class="nav-item nav-link active" href="#">Pagina de inicio </a>
+           <a class="nav-item nav-link" href="Informacion.php">Informacion sobre nosotros</a>
                <a class="nav-item nav-link" href="#"></a>
                <a class="nav-item nav-link" href="vista_alumnos.php">Alumnos</a>
                <a class="nav-item nav-link" href="#"></a>
                <a class="nav-item nav-link" href="vistas_cusos.php">Cursos</a>
                <a class="nav-item nav-link" href="#"></a>
-               <a class="nav-item nav-link" href="Informacion.php">Informacion sobre nosotros</a>
-               <a class="nav-item nav-link" href="#"></a>
-               <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
+               
+               <a class="nav-item nav-link" href="cerrar.php">Cerrar Sesion</a>
             
            </div>
        </nav>
